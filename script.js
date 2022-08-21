c2df3d1379920a026d702db95e11b866ce92b46c
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-08-21 12:39:40 +0200

    Finished Phase 3 of Project 1 finally

--- script.js
+++ script.js
@@ -1,113 +1,142 @@
-window.onload = function () {
-  let courses;
-  fetch("http://localhost:3000/courses")
-    .then((response) => response.json())
-    .then((json) => {
-      courses = json;
-      console.log(courses);
-      displayData(courses);
-    })
-    .catch(function (e) {
-      console.log("Error", e);
-    });
-  let old_section;
-  let data;
-  function displayData(courses) {
-    // deleting old data
-    old_section = document.querySelector("#courses-cards-section");
-    old_section.innerHTML = "";
-    data = courses;
-    for (let i = 0; i < data.length; i++) {
-      createElement(i);
-    }
-  }
-  function createElement(i) {
-    // adding course
-    const new_course = document.createElement("div");
-    new_course.classList.add("course");
-    old_section.appendChild(new_course);
-
-    // adding card
-    const new_card = document.createElement("div");
-    new_card.classList.add("card-img");
-    new_course.appendChild(new_card);
-
-    // adding image
-    const new_img = document.createElement("img");
-    new_img.src = data[i].image;
-    new_img.classList.add("course-img");
-    new_card.appendChild(new_img);
-
-    //Creating title
-    const h4 = document.createElement("h4");
-    h4.textContent = data[i].title;
-    h4.classList.add("course-caption");
-    new_card.appendChild(h4);
-
-    // Creating Author
-    const author = document.createElement("p");
-    author.textContent = data[i].instructors[0].name;
-    new_card.appendChild(author);
-
-    //Creating Rating Div
-    new_card.appendChild(addRating(data[i].rating));
-  }
-
-  function addRating(rating) {
-    rating = Math.round(rating * 10) / 10;
-    const div = document.createElement("div");
-    const span1 = document.createElement("span");
-    span1.textContent = rating;
-    span1.classList.add("stars");
-    div.appendChild(span1);
-    for (let i = 0; i < 4; i++) {
-      const span = document.createElement("span");
-      span.classList.add("bi");
-      span.classList.add("bi-star-fill");
-      div.appendChild(span);
-    }
-    const span = document.createElement("span");
-    span.classList.add("fa");
-    span.classList.add("fa-star-half-full");
-    span.classList.add("stars");
-    div.appendChild(span);
-    return div;
-  }
-  function search(value) {
-    old_section.innerHTML = "";
-    for (let i = 0; i < data.length; i++) {
-      let title = data[i].title.toLowerCase();
-      value = value.toLowerCase();
-      if (title.indexOf(value) != -1) {
-        createElement(i);
-        console.log(i);
-        console.log(title);
-      }
-    }
-  }
-  const search_button = document.querySelector("#searchbutton");
-  const search_bar = document.querySelector("#search-input");
-  search_button.addEventListener("click", () => {
-    const search_val = search_bar.value;
-    console.log(search_val);
-    search(search_val);
-  });
+/**
+ * This function fetches all courses from JSON server
+ *
+ * @param {String} courseName - The courses with this topic name
+ * @returns {Promise} - A promise that contains all fetched courses from JSON Server
+ */
+const fetchCourses = async (courseName) => {
+  console.log(courseName.replace(" ", ""));
+  const response = await fetch(
+    `http://localhost:3000/${courseName.replace(" ", "")}`
+  );
+  const courses = await response.json();
+  return courses;
 };
 
-// <div class="course">
-// <div class="card-img">
-//     <img src="https://img-c.udemycdn.com/course/240x135/394676_ce3d_5.jpg" alt="python" />
-// </div>
-// <h4>Learn Python: The Complete <br> Python Programming Course</h4>
-// <p class="author">Avinash Jain, The Codex</p>
-// <div class="rating">
-//     <span class="stars">4.4</span>
-//     <span class="bi bi-star-fill"></span>
-//     <span class="bi bi-star-fill"></span>
-//     <span class="bi bi-star-fill"></span>
-//     <span class="bi bi-star-fill"></span>
-//     <span class="bi bi-star-half"></span>
-//     <span class="grey">(17.972)</span>
-// </div>
-// <span class="price">E£199.99 <s>E£679.99</s></span>
-// </div>
+/**
+ *
+ * @param {Array} data - Array contains all courses
+ * @param {Integer} n - The size of group
+ * @returns {2D Array} - Array contains a group of courses represented by arrays
+ */
+function groupCourses(data, n) {
+  var group = [];
+  for (var i = 0, j = 0; i < data.length; i++) {
+    if (i >= n && i % n === 0) j++;
+    group[j] = group[j] || [];
+    group[j].push(data[i]);
+  }
+  return group;
+}
+
+/**
+ * A function that builds the HTML string of the course
+ *
+ * @param {Object} course - An Object that contains a single course information
+ * @returns {String} - A string of HTML representation of a course
+ */
+const buildCourse = (course) => {
+  const courseHTMLStr = `
+          <div class="course">
+              <img src="${course.image}" alt="${course.alt}" height="135">
+              <a href="${course.image}">
+                  <h4 class="course-title">${course.title}</h4>
+              </a>
+              <p class="course-instructor">${course.instructors[0].name}</p>
+              <div class="stars">
+                  <i class="course-rate">${
+                    Math.round(course.rating * 10) / 10
+                  }</i>
+                  <i class="fa fa-star checked"></i>
+                  <i class="fa fa-star checked"></i>
+                  <i class="fa fa-star checked"></i>
+                  <i class="fa fa-star checked"></i>
+                  <i class="fa-solid fa-star-half-stroke"></i>
+              </div>
+              <h4 class="course-price">E&#163;${course.price}</h4>
+          </div>
+        `;
+  return courseHTMLStr;
+};
+
+/**
+ *
+ * @param {Array} courses - An array of courses Objects
+ * @param {String} filterString - A string to filter the courses according their title (if empty then fetch all courses)
+ * @returns {String} - A string that contains tha HTML for all courses
+ */
+const showCourses = (courses, filterString) => {
+  // holds the result html for all courses
+  let coursesDiv = "";
+
+  // Filter courses according to the search string
+  courses = courses.filter(
+    (course) => course.title.toUpperCase().indexOf(filterString) > -1
+  );
+
+  // Group courses for each slide
+  const coursesGroups = groupCourses(courses, 5);
+
+  // build each slide of html
+  for (let i = 0; i < coursesGroups.length; i++) {
+    coursesDiv += `<div class="carousel-item ${i === 0 ? "active" : ""}">`;
+    coursesDiv += `<div class="courses" id="courses">`;
+    coursesGroups[i].forEach((course) => {
+      coursesDiv += buildCourse(course);
+    });
+    coursesDiv += `</div>`;
+    coursesDiv += `</div>`;
+  }
+
+  return coursesDiv;
+};
+
+/**
+ * @type {Array} - Carry all filtered fetched courses
+ */
+let courses;
+
+/**
+ * Fetch and show courses on load
+ */
+window.onload = async () => {
+  const courseName = document
+    .querySelector(".course-item.active")
+    .textContent.toLowerCase();
+  courses = await fetchCourses(courseName);
+  const courseSection = document.querySelector("#courses");
+  courseSection.innerHTML = showCourses(courses, "");
+};
+
+/**
+ * An on click handler for search input that takes the current value from search input and search for it
+ */
+document.querySelector("#search-input").addEventListener("click", (event) => {
+  event.preventDefault();
+  const filterString = document.querySelector("#search-input");
+  const courseSection = document.querySelector("#courses");
+  courseSection.innerHTML = showCourses(
+    courses,
+    filterString.value.toUpperCase()
+  );
+});
+
+// Add an event listener to each one of courses list
+const coursesNames = document.querySelectorAll(".course-item");
+for (let i = 0; i < coursesNames.length; i++) {
+  coursesNames[i].addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    // Get topic name to fetch it's courses from database
+    const newCourseName = coursesNames[i].textContent;
+    courses = await fetchCourses(newCourseName);
+    const courseSection = document.querySelector("#courses");
+    courseSection.innerHTML = showCourses(courses, "");
+
+    // Add active class to selected topic and remove it from other topics
+    coursesNames[i].classList.add("active");
+    for (let j = 0; j < coursesNames.length; j++) {
+      if (j !== i) coursesNames[j].classList.remove("active");
+    }
+  });
+}
